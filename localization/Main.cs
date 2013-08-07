02fa81322dec7173d544b5051a3c4248bc072f7b
o.joseph <o.joseph@gloops.com>
2013-08-07 14:38:14 +0900

    Tried many things
    Tried several approaches. I decided to go with a class as it seems
    easier to approach. Need to clean the code a bit in order to make it
    more useful.

--- localization/Main.cs
+++ localization/Main.cs
@@ -29,19 +29,92 @@ namespace Dictionary{
 		
 		public static void Main (string[] args){	
 			Console.WriteLine ("Hello World!");
-			Dictionary<registeredKeys, string> theMainDict = new Dictionary<registeredKeys, string>();
+			//Dictionary<registeredKeys, string> theMainDict = new Dictionary<registeredKeys, string>();
 			//theMainDict.Add(registeredKeys.gameMultiplayer, "MultiJoueur");
-			theMainDict.Add(registeredKeys.gameMultiplayer, "multijoueur");
+			/*theMainDict.Add(registeredKeys.gameMultiplayer, "multijoueur");
 			theMainDict.Add(registeredKeys.gameStage, "Niveau");
 			theMainDict.Add(registeredKeys.gameStartNew, "Commencer");
-			theMainDict.Add(registeredKeys.gameExit, "Quitter");
+			theMainDict.Add(registeredKeys.gameExit, "Quitter");*/
 			
+			//Dictionary<registeredKeys, languageRegion> segaSonic = new Dictionary<registeredKeys, languageRegion>();
+		
 			
-			//Here we test the output
-			Console.WriteLine(theMainDict[registeredKeys.gameExit]);
+			//pokemon Pikachu = new pokemon(25,"Pikachu",35,55,30,90,pokemon.theType.electric, PikachuMVList);
+			keywords wordContinue = new keywords(keywords.rgistKey.gameContinue,"Continuer","Continue","Tsuzuku");
+			wordContinue.getTheObjct(keywords.lngRegion.fr_can,wordContinue);
 		}
 		
 		
 		
+		//Fonction avec 2 parametres
+		// Param = (le mot clef,   la langue/Region)
+		//On cherches dans un tableau en utilisant la clef.
+		//On cherches pour la bonne traduction en utilisant le deuxieme parametre
+		//On retourne le resultat 
+		
+		//List<string> listKeyword = new List<string>();
+		
+		
+		
 	}
+	
+	class keywords{
+		
+		public rgistKey kwrdKey;
+		//public lngRegion kwrdRegion;
+		public string kwrdFr;
+		public string kwrdEng;
+		public string kwrdJp;
+		
+		
+		//First we want to has a bank that holds all the possible keys.
+		public enum rgistKey{
+			gameContinue,
+			gameStage,
+			gameStartNew,
+			gameExit,
+			gameMultiplayer
+		}
+		
+		//Here we store the different Languages + Regions
+		public enum lngRegion{
+			jp,
+			en_us,
+			en_uk,
+			fr_fr,
+			fr_can,
+			spn_spn,
+			spn_mex
+		}
+		
+		//Constructor
+		public keywords(rgistKey theKey, /*lngRegion theRegion, */string txtFr, string txtEng, string txtJp){
+			
+			kwrdKey = theKey;
+			//kwrdRegion = theRegion;
+			kwrdFr = txtFr;
+			kwrdEng = txtEng;
+			kwrdJp = txtJp;
+			
+		}
+		
+		public void getTheObjct(lngRegion wantedRegion, keywords theKeyWord){
+			
+			switch(wantedRegion){
+				case lngRegion.en_us:
+					Console.WriteLine("US: " + theKeyWord.kwrdEng);
+				break;
+				case lngRegion.jp:
+					Console.WriteLine("JAPAN: " + theKeyWord.kwrdJp);
+				break;
+				case lngRegion.fr_can:
+					Console.WriteLine("CANADA: " + theKeyWord.kwrdFr);
+				break;
+			}
+			
+		}
+		
+	}
+	
+	
 }
